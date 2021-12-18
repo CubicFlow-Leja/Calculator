852fb93b4ba6cbd7f280b82dbb1d5f1e2d0e70cf
CubicFlow <lsdavinci82gmail.com>
2021-12-18 09:12:32 +0100

    first commit

---
+++ src/Components/Key.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+interface KeyProps {
+  keySign: string;
+  keyCallback: Function;
+}
+
+const Key: React.FC<KeyProps> = () => {
+  return <button></button>;
+};
+
+export default Key;
